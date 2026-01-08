17b3287be0da43c4bd93057091f87cb0afe5d8ab
Ben Kostka <mail@ben-kostka.de>
2026-01-08 19:43:09 +0100

    tasked solved

--- Arbeitsblätter/ab3/301a_array_initialisierung/p.c
+++ Arbeitsblätter/ab3/301a_array_initialisierung/p.c
@@ -2,8 +2,10 @@
 
 int main()
 {
+    int array[10] = {1,2,3,4,5};
 
-
-
+    for(int i = 0; i < 10;i++){
+        printf("%i\n", array[i]);
+    }
 
 }
--- Arbeitsblätter/ab3/301b_zeichenkette_großschreibung/p.c
+++ Arbeitsblätter/ab3/301b_zeichenkette_großschreibung/p.c
@@ -2,13 +2,14 @@
 
 int main()
 {
-
-
-
-
-
-
-
-
+    char str[24] = "Heute ist ein guter Tag!";
+    
+    for(int i = 0; i < 24; i++){
+        if (str[i] <= 0x7A && str[i] >= 0x61){
+            printf("%c", (str[i]-32));
+        } else {
+            printf("%c", str[i]);
+        }
+    }
 
 }
