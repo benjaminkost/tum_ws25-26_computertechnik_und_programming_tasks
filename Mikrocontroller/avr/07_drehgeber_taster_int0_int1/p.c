a8283af2dac518d229106c3dbfff8a84106bafb1
Ben Kostka <mail@ben-kostka.de>
2026-01-12 19:17:57 +0100

    task 11

--- Mikrocontroller/avr/07_drehgeber_taster_int0_int1/p.c
+++ Mikrocontroller/avr/07_drehgeber_taster_int0_int1/p.c
@@ -1,40 +1,33 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-
 #define SET_LED_ON (PORTD |= 1)
 #define SET_LED_OFF (PORTD &= ~1)
 #define TOGGLE_LED (PORTD ^= 1)
 
 ISR(INT0_vect)
 {
-
-
-
-
-
-
+    if(PIND & 2) // links gedreht
+        SET_LED_OFF;
+    else // rechts gedreht
+        SET_LED_ON;
 }
 
 ISR(INT1_vect)
 {
-	TOGGLE_LED;
+    TOGGLE_LED;
 }
 
 int main(void)
 {
+    DDRD = 1; // PD0 als Ausgang festlegen (LED)
 
+    SREG = 128; // Interrupts global freischalten
 
+    EICRA = 8 | 3; // 8: INT1 fallende Flanke;
+                   // 3: INT0 steigende Flanke
 
+    EIMSK = 3; // INT0 und INT1 freischalten
 
-
-
-
-
-
-
-
-
-
-
+    while(1); // warten
 }
--- Mikrocontroller/avr/08_t1_int_pre_1024/p.c
+++ Mikrocontroller/avr/08_t1_int_pre_1024/p.c
@@ -7,19 +7,23 @@
 
 ISR(TIMER1_OVF_vect)
 {
-
-
-
+    TCNT1 = 65536 - 19531; // siehe unten
+    TOGGLE_LED;
 }
-
 
 int main(void)
 {
+    DDRD=0x01; // set PD0 as output
+    SREG = 0x80; // globally enable interrupts
+    TIMSK1 = 0x01; // enable Timer 1 interrupt
+    TCCR1B = 0x05; // prescaler 1024
 
+    // Frequenz: 20 MHz / 1024 = 19.531
+    // d.h. 19.531 Taktzyklen pro Sekunde
+    // Jede Sekunde, d.h. alle 19.531 Takte
+    // soll ein Timer-Überlauf stattfinden
+    // => von 65.536 19.531 abziehen
+    TCNT1 = 65536 - 19531;
 
-
-
-
-
-
+    while(1);
 }
