3d91941872aba22d93a826c159beabbeb0bca776
Ben Kostka <mail@ben-kostka.de>
2026-01-15 20:36:11 +0100

    305-306

--- Arbeitsblätter/ab3/305c_array_doppelte_eintraege/p.c
+++ Arbeitsblätter/ab3/305c_array_doppelte_eintraege/p.c
@@ -4,37 +4,30 @@
 int main()
 {
 	// Deklarationen
-
-
-
+	int a[700];
+	int n,i,j;
+	bool found;
 
 
 	// Matrikelnummern einlesen; eine eingele-
 	// sene 0 soll den Einleseprozess beenden
+	for(n=0; n < 700;n++){
+		printf("Matrikelnummer: ");
+		scanf("%i", a+n);
 
-
-
-
-
-
+		if(a[n]==0) break;
+	}
 
 
 	// Matrikelnummern ausgeben, dabei jede bereits
 	// ausgegebene Nummer nicht noch einmal ausgeben
+	for(i= 0;i < 700;i++){
+		for(j=0; j<i;j++){
+			if(a[j] == a[i]) found = 1;
+		}
+		if(!found && a[i]!=0) printf("%i\n", a[i]);
+		found = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		if(a[i]==0) break;
+	}
 } // Ende main-Routine
--- Arbeitsblätter/ab3/306c_roemisch/p.c
+++ Arbeitsblätter/ab3/306c_roemisch/p.c
@@ -3,53 +3,85 @@
 int main()
 {
 	// Deklarationen
-					// Array "rds" für 15 "roman digits" (Zeichen)
-					// Array "vs" ("values") für die den eingegebenen  
-					// römischen Zeichen entsprechenden Festkomma-Werte
-					// Variable "sum" um die Werte aufzuaddieren
-					// Variable l speichert die Länge der Zeichenkette
+	// Array "rds" für 15 "roman digits" (Zeichen)
+	char rds[16];	
+	// Array "vs" ("values") für die den eingegebenen
+	// römischen Zeichen entsprechenden Festkomma-Werte
+	int vs[16];
+
+	// Variable "sum" um die Werte aufzuaddieren
+	int sum = 0;
+	// Variable l speichert die Länge der Zeichenkette
+	int l=0;
 
 	// Römische Zahl einlesen
-
-
+	printf("Römische Zahl: ");
+	scanf("%15s", rds);
 	
 	// Werte der einzelnen römischen Ziffern im Array "rds" ermitteln
 	// und an den entsprechenden Positionen im Array "vs" ablegen
 	// Variable l soll danach die Anzahl römischer Ziffern enthalten.
 
 	// rds in vs umwandeln; l soll danach die Länge enthalten
-
-
-
-
-
-
-
-
-
-
-
+	for(int i = 0; rds[i]; i++){
+		switch (rds[i])
+		{
+		case 'I': 
+			vs[i] = 1;
+			break;
+		case 'V': 
+			vs[i] = 5;
+			break;
+		case 'X': 
+			vs[i] = 10;
+			break;
+		case 'L': 
+			vs[i] = 50;
+			break;
+		case 'C': 
+			vs[i] = 100;
+			break;
+		case 'D': 
+			vs[i] = 500;
+			break;
+		case 'M': 
+			vs[i] = 1000;
+			break;
+		case 'i': 
+			vs[i] = 1;
+			break;
+		case 'v': 
+			vs[i] = 5;
+			break;
+		case 'x': 
+			vs[i] = 10;
+			break;
+		case 'l': 
+			vs[i] = 50;
+			break;
+		case 'c': 
+			vs[i] = 100;
+			break;
+		case 'd': 
+			vs[i] = 500;
+			break;
+		case 'm': 
+			vs[i] = 1000;
+			break;
+		default:
+			break;
+		}
+		l++;
+	}
 
 	// Wert berechnen und in Variable sum ablegen
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	for(int i = 0; i < l;i++){
+		if (i != 0 && (vs[i-1] < vs[i])){
+			sum += (vs[i] -2*vs[i-1]);
+		} else{
+			sum += vs[i]; //
+		}
+	}
 
 	printf("Der Wert ist %d.\n", sum);
 } // main
